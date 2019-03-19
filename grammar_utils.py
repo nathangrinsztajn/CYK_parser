a7a32da35d5e7a98fde91dfe94ff1e5a7b26fc78
Ultiss <nathan.grinsztajn@polytechnique.edu>
2019-03-19 23:56:30 +0100

    lots of work

---
+++ grammar_utils.py
@@ -0,0 +1,26 @@
+def normalize_grammar(g):
+    """
+    :param g: grammar (dict of dict of count)
+    :return: normalized grammar (dict of dict of probability)
+    """
+    for pos in g.keys():
+        count = sum(g[pos].values())
+        for key in g[pos].keys():
+            g[pos][key] /= count
+    return g
+
+def remove_unit_grammar(grammar):
+    """
+    :param grammar: grammar (dict of dict of count)
+    :return: grammar with no unary rule
+    """
+    still_unit = False
+    for pos in grammar:
+        for key in list(grammar[pos]):
+            if len(key)==1:
+                proba = grammar[pos].pop(key)
+                for new_key in list(grammar.get(key[0], {})):
+                    grammar[pos][new_key] = grammar[pos].get(new_key, 0)
+                    grammar[pos][new_key] = grammar[pos][new_key] + proba*grammar[key[0]][new_key]
+                still_unit = True
+    return grammar, still_unit
